233d9d48fbab272d3661b550c177b2206f08c96f
Ivan Korobkov <ivan.korobkov@gmail.com>
2010-06-14 15:10:32 +0400

    Removed scopes from invokers tests.

--- Source/inject_tests/test_invokers.py
+++ Source/inject_tests/test_invokers.py
@@ -1,23 +1,26 @@
-from inject import scopes
 import unittest
+from mock import Mock
 
 from inject.invokers import Invoker
 
 
 class InvokerTestCase(unittest.TestCase):
     
-    invoker_class = Invoker
+    def setUp(self):
+        class DummyInvoker(Invoker):
+            injection_class = Mock()
+        
+        self.invoker_class = DummyInvoker
     
     def testNew(self):
-        '''Invoker() should return an instance if an unbound method.'''
+        '''Invoker should return an instance if an unbound method.'''
         class A(object):
             def method(self): pass 
         invoker = self.invoker_class(A.method)
         self.assertTrue(isinstance(invoker, self.invoker_class))
     
     def testNewNotUnboundMethod(self):
-        '''Invoker() should return an obj untouched if not an unbound method.
-        '''
+        '''Invoker should return an obj untouched if not an unbound method.'''
         def func():
             pass
         invoker = self.invoker_class(func)
@@ -25,34 +28,25 @@ class InvokerTestCase(unittest.TestCase):
         
         class A(object):
             def method(self): pass
+        
         a = A()
         invoker = self.invoker_class(a.method)
+        
         self.assertEqual(invoker, a.method)
     
     def testInvoke(self):
-        '''Invoker.__call__() should inject a class and call a method.'''
+        '''Invoker should inject a class and call a method.'''
         class A(object):
             def method(self, arg):
                 return 'arg: %s' % arg
+        
+        a = A()
         invoker = self.invoker_class(A.method)
+        invoker.injection.get_instance.return_value = a
+        
         result = invoker('value')
         self.assertEqual(result, 'arg: value')
-    
-    def testInvokeScope(self):
-        '''Invoker.__call__() should inject a class using a provided scope.'''
-        class A(object):
-            i = 0
-            def __init__(self):
-                self.__class__.i += 1
-            def method(self):
-                return self.i
-        invoker = self.invoker_class(A.method, scope=scopes.app)
-        result1 = invoker()
-        result2 = invoker()
-        self.assertEqual(result1, 1)
-        self.assertEqual(result2, 1)
-        scopes.app.clear()
-    
+        
     def testHash(self):
         '''Invoker should have the same hash an unbound method.'''
         class A(object):
