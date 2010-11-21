23a672d5a588c1de4bbf7cf5e9f9ce02ee62518c
Ivan Korobkov <ivan.korobkov@gmail.com>
2010-11-21 18:09:13 +0300

    Renamed the project from Inject into python-inject.

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from distutils.core import setup
 
 
 setup(
-      name='Inject',
+      name='python-inject',
       version='2.0-alpha',
       url='http://code.google.com/p/python-inject/',
       author='Ivan Korobkov',
       author_email='ivan.korobkov@gmail.com',
-      description='Fast python dependency injection',
+      description='Python dependency injection framework',
       license='MIT License',
-      package_dir={'': 'Source'},
+      package_dir={'': 'src'},
       packages=['inject'],
       keywords=['injection', 'ioc', 'inversion of control',
-                  'dependency injection', 'loose coupling'],
+                'dependency injection', 'loose coupling'],
       classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
