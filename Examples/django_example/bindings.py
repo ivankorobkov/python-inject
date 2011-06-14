f8a01369fbf4b766ddb2db97e08e3b8070e9d91f
Ivan Korobkov <ivan.korobkov@gmail.com>
2011-06-14 12:29:33 +0400

    Added db engine configuration to django_example.

--- Examples/django_example/bindings.py
+++ Examples/django_example/bindings.py
@@ -3,11 +3,17 @@ import inject
 from datetime import datetime
 
 
-# Create an injector and add bindings.
+# Create and REGISTER it!
 injector = inject.Injector()
+inject.register(injector)
+
+# Add all your bindings.
+
+# This dependency is instantiated only once for the whole application.
 injector.bind('app_started_at', to=datetime.now, scope=inject.appscope)
+
+# Once for every request.
 injector.bind('req_started_at', to=datetime.now, scope=inject.reqscope)
 
-# REGISTER THE INJECTOR!
-# So that it is used to get instances.
-inject.register(injector)
+# Every time when accessed.
+injector.bind('req_ended_at', to=datetime.now)
