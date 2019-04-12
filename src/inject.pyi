55106b2451802db42ab165b496bbcd5494b3b3ee
SixExtreme <943459049@qq.com>
2019-04-12 18:06:51 +0800

    fix: define Binder and Injector in inject.pyi

--- src/inject.pyi
+++ src/inject.pyi
@@ -1,23 +1,61 @@
-import typing
+from typing import Type, TypeVar, Callable, Optional
 
 
-T = typing.TypeVar('T')
-Binder = typing.TypeVar('Binder')
-Injector = typing.TypeVar('Injector')
+T = TypeVar('T')
+
+
+class Binder(object):
+
+    def __init__(self) -> None: ...
+
+    def install(
+        self,
+        config: Callable[[Binder], None]
+    ) -> Binder: ...
+
+    def bind(self,
+        cls: Type[T],
+        instance: T
+    ) -> Binder: ...
+
+    def bind_to_constructor(
+        self,
+        cls: Type[T],
+        constructor: Callable
+    ) -> Binder: ...
+
+    def bind_to_provider(
+        self,
+        cls: Type[T],
+        provider: Callable
+    ) -> Binder: ...
+
+    def _check_class(self, cls: Type[T]) -> None: ...
+
+
+class Injector(object):
+
+    def __init__(
+        self,
+        config: Optional[Callable[[Binder], None]] = None,
+        bind_in_runtime: bool = True
+    ) -> None: ...
+
+    def get_instance(self, cls: Type[T]) -> T: ...
 
 
 def configure(
-    config: typing.Optional[typing.Callable], bind_in_runtime: bool = True
+    config: Optional[Callable], bind_in_runtime: bool = True
 ) -> None: ...
 
 
 def configure_once(
-    config: typing.Optional[typing.Callable], bind_in_runtime: bool = True
+    config: Optional[Callable], bind_in_runtime: bool = True
 ) -> None: ...
 
 
 def clear_and_configure(
-    config: typing.Optional[typing.Callable], bind_in_runtime: bool = True
+    config: Optional[Callable], bind_in_runtime: bool = True
 ) -> None: ...
 
 
@@ -27,19 +65,19 @@ def is_configured() -> bool: ...
 def clear() -> None: ...
 
 
-def params(**args_to_classes: typing.Type[T]) -> typing.Callable: ...
+def params(**args_to_classes: Type[T]) -> Callable: ...
 
 
-def autoparams(*selected_args: str) -> typing.Callable: ...
+def autoparams(*selected_args: str) -> Callable: ...
 
 
-def instance(cls: typing.Type[T]) -> T: ...
+def instance(cls: Type[T]) -> T: ...
 
 
-def attr(cls: typing.Type[T]) -> T: ...
+def attr(cls: Type[T]) -> T: ...
 
 
-def get_injector() -> typing.Optional[Injector]: ...
+def get_injector() -> Optional[Injector]: ...
 
 
 def get_injector_or_die() -> Injector: ...
