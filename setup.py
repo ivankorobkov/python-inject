ba2fa66c334515cd5826410ff1edd880f3338f9a
Ivan Korobkov <ivan.korobkov@gmail.com>
2019-07-10 11:24:08 +0300

    Dev version 3.5.3.dev0.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read_description():
 
 setup(
     name='Inject',
-    version='3.5.2',
+    version='3.5.3.dev0',
     url='https://github.com/ivankorobkov/python-inject',
     license='Apache License 2.0',
 
