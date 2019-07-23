d5fc1d0bd5739232d0b5c98074e3f3b536fad333
jmeyer <joshua.meyer@charter.com>
2019-07-23 13:31:05 -0600

    switched apostrephe with quotation to match style

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
         )
     ],
 
-    install_requires=["typing; python_version<'3.5'"],
+    install_requires=['typing; python_version<"3.5"'],
 
     classifiers=[
         'Development Status :: 5 - Production/Stable',
